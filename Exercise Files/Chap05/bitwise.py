9f37627bb5386cb61cfc3540ecd183087a3caf6a
Dick de Goede <dick.degoede@park-now.com>
2021-07-14 12:40:06 +0200

    updates

--- Exercise Files/Chap05/bitwise.py
+++ Exercise Files/Chap05/bitwise.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-x = 0x0a
+x = 0xef
 y = 0x02
-z = x & y
 
+z = x & y  # and
+q = x | y  # or
+r = x ^ y  # xor
+s = x << y # shift left
+t = x >> y # shift right
+
+print('AND')
 print(f'(hex) x is {x:02x}, y is {y:02x}, z is {z:02x}')
 print(f'(bin) x is {x:08b}, y is {y:08b}, z is {z:08b}')
+print(f'(dec) x is {x:02d}, y is {y:02d}, z is {z:02d}')
+print ('\nOR')
+print(f'(bin) x is {x:08b}, y is {y:08b}, q is {q:08b}')
+print(f'(dec) x is {x:02d}, y is {y:02d}, q is {q:02d}')
+print ('\nXOR')
+print(f'(bin) x is {x:08b}, y is {y:08b}, r is {r:08b}')
+print(f'(dec) x is {x:02d}, y is {y:02d}, r is {r:02d}')
+print ('\nShift left')
+print(f'(bin) x is {x:08b}, y is {y:08b}, s is {s:08b}')
+print(f'(dec) x is {x:02d}, y is {y:02d}, s is {s:02d}')
+print ('\nShift right')
+print(f'(bin) x is {x:08b}, y is {y:08b}, t is {t:08b}')
+print(f'(dec) x is {x:02d}, y is {y:02d}, t is {t:02d}')
--- Exercise Files/Chap05/hello.py
+++ Exercise Files/Chap05/hello.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-print('Hello, World.')
+# Python3 program to illustrate
+# working of AND gate
+ 
+def AND (a, b):
+ 
+    if a == 1 and b == 1:
+        return 'True'
+    else:
+        return 'False'
+ 
+# Driver code
+if __name__=='__main__': 
+ 
+    print("+-----------------------------------+")
+    print("| AND Truth Table      |     Result |")
+    print("+----------------------+------------+")
+    print(f'| A = False, B = False | {AND(False, False):>10} |')
+    print(f'| A = True,  B = False | {AND(True, False):>10} |')
+    print(f'| A = False, B = True  | {AND(False, True):>10} |')
+    print(f'| A = True,  B = True  | {AND(True, True):>10} |')
+    print("+----------------------+------------+")
+
