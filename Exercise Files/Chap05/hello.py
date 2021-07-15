3c3c58e7eb3f56c5acc2b7d3d983d6176959fb7a
Dick de Goede <dick.degoede@park-now.com>
2021-07-15 08:59:14 +0200

    small updates

--- Exercise Files/Chap05/hello.py
+++ Exercise Files/Chap05/hello.py
@@ -2,15 +2,16 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 # Python3 program to illustrate
-# working of AND gate
- 
-def AND (a, b):
- 
-    if a == 1 and b == 1:
-        return 'True'
-    else:
-        return 'False'
- 
+# working of different logical gates
+
+def NOT  (a):    return 'True' if not a else 'False'
+def AND  (a, b): return 'True' if a and b else 'False'
+def NAND (a, b): return 'True' if not(a and b) else 'False'
+def OR   (a, b): return 'True' if a or b else 'False'
+def NOR  (a, b): return 'True' if not(a or b) else 'False'
+def XOR  (a, b): return 'True' if a !=b  else 'False'
+def XNOR (a, b): return 'True' if not(a !=b)  else 'False'       # The XNOR gate (negated XOR) gives an output of 1 both inputs are same and 0 if both are different.
+
 # Driver code
 if __name__=='__main__': 
  
