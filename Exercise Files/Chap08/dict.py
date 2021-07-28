a29073dbd318945645acb3e6a5fb30ef76fdc0cf
Dick de Goede <dick.degoede@park-now.com>
2021-07-28 10:21:15 +0200

    updates

--- Exercise Files/Chap08/dict.py
+++ Exercise Files/Chap08/dict.py
@@ -2,11 +2,15 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 def main():
-    animals = { 'kitten': 'meow', 'puppy': 'ruff!', 'lion': 'grrr',
-        'giraffe': 'I am a giraffe!', 'dragon': 'rawr' }
+    animals = { 'kitten': 'meow', 'puppy': 'ruff!', 'lion': 'grrr', 'giraffe': 'I am a giraffe!', 'dragon': 'rawr' }
+    # or
+    animals = dict(kitten='meow', puppy='ruff!', lion='grrr', giraffe='I am a giraffe!', dragon='rawr')
+
     print_dict(animals)
 
 def print_dict(o):
     for x in o: print(f'{x}: {o[x]}')
+    # or
+    for k, v in o.items(): print(f'{k}: {v}')
 
 if __name__ == '__main__': main()
--- Exercise Files/Chap08/lists.py
+++ Exercise Files/Chap08/lists.py
@@ -3,7 +3,23 @@
 
 def main():
     game = [ 'Rock', 'Paper', 'Scissors', 'Lizard', 'Spock' ]
+
+    game.append('Computer')
+    game.remove('Lizard')
+    game.insert(0, 'First') # Insert at first position (0), lists are 0-based
+
     print_list(game)
+    x = game.pop(); print(x)
+    print_list(game)
+
+    print(', '.join(game))
+    print(len(game))
+
+    print(game[0])
+    print(game[1:3])
+
+    i = game.index('Spock'); print(i)
+
 
 def print_list(o):
     for i in o: print(i, end=' ', flush=True)
